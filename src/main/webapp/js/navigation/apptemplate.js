c3796cff1e0e5fa53035a74141f2f4813d2ebd84
liuzongfeng <lzf75585@163.com>
2017-05-15 14:17:22 +0800

    模板js

--- src/main/webapp/js/navigation/apptemplate.js
+++ src/main/webapp/js/navigation/apptemplate.js
@@ -1,7 +1,90 @@
 angular.module('apptemplate', ['ngRoute', 'auth']).controller(
-		'apptemplate',
+		'apptemplate', 
 
 		function($route,$http, auth,$rootScope,$scope) {
+			
+			//发起请求加载模板列表
+			selectPage_aa = function (page) {
+				
+				$http({
+					  method: 'GET',
+					  params:{"pageNo":page,"pageSize":2},
+					  url: 'http://localhost:8080/obtainTemplateList'
+					}).then(function successCallback(response) {
+						
+						$scope.templates = response.data.list;   //要展示的数据
+						
+						$scope.totalSize = response.data.total;   //共多少条数据
+						$scope.pageSize = response.data.pageSize;  //每页条数
+						$scope.pages = response.data.pages; //共多少分页数
+						
+						$scope.newPages = $scope.pages > 5 ? 5 : $scope.pages; //按钮数超过5个，按5个显示
+						$scope.pageList = [];                          //页码集合
+						$scope.selPage = page;                    //当前页
+						
+						//分页要repeat的数组
+						for (var i = 0; i < $scope.newPages; i++) { //处理按钮数少于5个的页码集合
+							$scope.pageList.push(i + 1);
+						}
+						
+						//当前页不能小于1，大于最大
+						if (page < 1 || page > $scope.pages) return;
+						
+						//最多显示分页数5
+						if (page > 2) {
+						//因为只显示5个页码，大于2页开始分页转换
+							var newpageList = [];
+							var i = (page - 3);
+							if($scope.pages - i < 5){
+								i = $scope.pages -5;
+							}
+							for ( ; i < ((page + 2) > $scope.pages ? $scope.pages : (page + 2)) ; i++) {
+								newpageList.push(i + 1);
+							}
+							$scope.pageList = newpageList;
+						}
+						
+						//选中页
+						$scope.selectPage = function (page) {
+							$scope.selPage = page;     //设置当前页
+							$scope.isActivePage(page);//选中样式
+							
+							selectPage_aa(page); //发起请求
+							console.log("选择的页：" + page);
+						};
+						//设置当前选中页样式
+						$scope.isActivePage = function (page) {
+							return $scope.selPage == page;
+						};
+						//上一页
+						$scope.Previous = function () {
+							if($scope.selPage  == 1){
+								alert("第一页");
+							}else{
+								
+								$scope.selectPage($scope.selPage - 1);
+							}
+							
+						}
+						//下一页
+						$scope.Next = function () {
+							
+							
+							if($scope.selPage == $scope.pages){
+								alert("最后一页");
+							}else{
+								
+								$scope.selectPage($scope.selPage + 1);
+							}
+							
+						};
+					  }, function errorCallback(response) {
+					    // called asynchronously if an error occurs
+					    // or server returns response with an error status.
+				});
+			};
+			//
+			selectPage_aa(1);//查询列表
 			
 			$scope.appTab= true;
 			$scope.apptemplate1_title = true;
@@ -11,15 +94,71 @@ angular.module('apptemplate', ['ngRoute', 'auth']).controller(
 			$("#activeTemplate").attr("class","active");
 			
 			console.log("2222222222222222");
+			
+			//导入模板
+			$scope.importTemplate_fn = function(){
+				var formData = new FormData($('#uploadForm')[0]);
+				formData.append("file1",$('#uploadForm')[0]);
+				//formData.append("file1",$('#uploadForm')[1]);
+				$.ajax({
+				    url: 'http://localhost:8080/testUploadFile',
+				    type: 'POST',
+				    cache: false,
+				    data: formData,
+				    processData: false,
+				    contentType: false
+				}).done(function(res) { 
+					if("uploadOK" == res){
+						
+						swal({   
+							title:"导入成功",
+							text: "是否继续导入?",   
+							type: "success",   
+							showCancelButton: true,   
+							confirmButtonColor: "#DD6B55",
+							cancelButtonText: "否",//取消按钮文本
+							confirmButtonText: "是",//确定按钮上面的文档
+							closeOnConfirm: false 
+						}, 
+						function(isUploadMore){
+							if(isUploadMore){
+								//请继续
+								//alert(cancelButtonText);
+								swal.close();
+							}else{
+								//1.将模态框进行隐藏
+								$("#file1").val("");
+								$("#myModal").modal('hide');
+								//2.发起查询模板列表的请求
+								alert("发起查询模板列表的请求");
+								
+							}
+						});
+					}else{
+						swal({   
+							title:"导入失败",
+							text: res,   
+							type: "error",   
+							closeOnConfirm: true 
+						}, 
+						function(){
+								//1.将模态框进行隐藏
+								$("#file1").val("");
+								$("#myModal").modal('hide');
+						});
+						
+						
+					}
+				});
+			}
+			
 			//应用模板
 			$scope.appTemplate_fn = function(){
 				alert("应用模板")
                 $("#activeTemplate").attr("class","active");
 				$("#activeEdit").attr("class","");
-
 				$scope.editTemplate = false;
 				//$scope.editTemplate_title=false;
-				
 				$scope.appTab= true;
 				$scope.apptemplate1_title = true;
 				$scope.apptemplate1=true;
@@ -64,7 +203,7 @@ angular.module('apptemplate', ['ngRoute', 'auth']).controller(
 			
 			//删除模板
 			$scope.delTemplate_fn = function(){
-				alert("删除模板");
+				//alert("删除模板");
 				
 				
 				swal({   
@@ -77,8 +216,8 @@ angular.module('apptemplate', ['ngRoute', 'auth']).controller(
 					confirmButtonText: "是的，确定删除！",//确定按钮上面的文档
 					closeOnConfirm: false 
 				}, 
-				function(){
-					
+				function(aa){
+					alert(aa);
 					alert("请求后端进行删除");
 					swal("删除成功!", "该项目已被删除!", "success");
 					/*if("" != projectId){
@@ -86,19 +225,6 @@ angular.module('apptemplate', ['ngRoute', 'auth']).controller(
 					}*/
 				});
 		}
-			//返回后的函数
-		/*function showManagerProject(result){
-			if("1" == result){
-				swal("删除成功!", "该项目已被删除!", "success");
-				setTimeout('tiaozhuanyemian()',1000);
-			}else if("0" == result){
-				swal("删除失败!", "请联系管理员!", "error");
-			}else if("2" == result){
-				swal("系统异常!", "请联系管理员!", "error"); 
-			}else{
-				swal("系统异常!", "请联系管理员!", "error");
-			}
-		}*/
 			
 			//导出模板
 			$scope.checkOutTemplate_fn = function(){
