ec6c5240f79b35cd1b2b47e7f4cf9e46f2cfba3f
liuzongfeng <lzf75585@163.com>
2017-05-17 12:13:33 +0800

    ss

--- src/main/java/rest/mybatis/dao/passDao/Imp/PaasTemplateImp.java
+++ src/main/java/rest/mybatis/dao/passDao/Imp/PaasTemplateImp.java
@@ -17,53 +17,83 @@ import rest.mybatis.model.passModel.PaasTemplate;
 import rest.page.util.Message;
 import rest.page.util.Pageinfo;
 @Configuration
-public class PaasTemplateImp implements PaasTemplateMapper {
+public class PaasTemplateImp{
 	@Autowired
 	private SqlSessionFactory sqlSessionFactory;
-	@Override
-	public int deleteByPrimaryKey(Integer id) {
-		// TODO Auto-generated method stub
-		return 0;
+	package rest.mybatis.dao.passDao.Imp;
+
+	import java.util.Date;
+	import java.util.HashMap;
+	import java.util.List;
+	import java.util.Map;
+
+	import org.apache.ibatis.session.SqlSession;
+	import org.apache.ibatis.session.SqlSessionFactory;
+	import org.mybatis.spring.support.SqlSessionDaoSupport;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.context.annotation.Bean;
+	import org.springframework.context.annotation.Configuration;
+
+	import rest.mybatis.dao.passDao.PaasTemplateMapper;
+	import rest.mybatis.model.passModel.PaasTemplate;
+	import rest.page.util.Message;
+	import rest.page.util.Pageinfo;
+	@Configuration
+	public class PaasTemplateImp{
+		@Autowired
+		private SqlSessionFactory sqlSessionFactory;
+
+		public PaasTemplate selectByPrimaryKey(Integer id) {
+			SqlSession session=sqlSessionFactory.openSession();
+			List<PaasTemplate> list=session.selectList("selectByid",id);
+			return list.size()>0?list.get(0):null;
+		}
+
+		public int selectByCondition(PaasTemplate record) {
+			SqlSession session=sqlSessionFactory.openSession();
+			List list=session.selectList("selectPaasTemplateByCondit", record);
+			session.close();
+			return list.size()>0?(Integer)list.get(0):0;
+		}
+		//安条件查询信息
+		public List<PaasTemplate> selectObjByCondition(Pageinfo record) {
+			SqlSession session=sqlSessionFactory.openSession();
+			List<PaasTemplate> list=session.selectList("selectPaasTemplateObjByCondit", record);
+			session.close();
+			return list;
+		}
+		//编辑产品信息
+		public Message updateProduct(PaasTemplate record) {
+			SqlSession session=sqlSessionFactory.openSession();
+			int result=session.update("UpdateProduct",record);
+			session.close();
+			if(result>0)
+			{
+				return new Message("success", "更新成功", new Date());
+			}else
+			{
+				return new Message("fail", "更新失败", new Date());
+			}
+		}
+
+		public Map<String, List> getTypeAndMode() {
+			Map<String, List> map=new HashMap<String, List>();
+			SqlSession session=sqlSessionFactory.openSession();
+			List Categorylist=session.selectList("selectCategory");
+			List Usermodelist=session.selectList("selectUserMode");
+			map.put("category", Categorylist);
+			map.put("usermode", Usermodelist);
+			session.close();
+			return map;
+		}
 	}
 
-	@Override
-	public int insert(PaasTemplate record) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int insertSelective(PaasTemplate record) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
 	public PaasTemplate selectByPrimaryKey(Integer id) {
 		SqlSession session=sqlSessionFactory.openSession();
 		List<PaasTemplate> list=session.selectList("selectByid",id);
 		return list.size()>0?list.get(0):null;
 	}
 
-	@Override
-	public int updateByPrimaryKeySelective(PaasTemplate record) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int updateByPrimaryKeyWithBLOBs(PaasTemplate record) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int updateByPrimaryKey(PaasTemplate record) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
 	public int selectByCondition(PaasTemplate record) {
 		SqlSession session=sqlSessionFactory.openSession();
 		List list=session.selectList("selectPaasTemplateByCondit", record);
@@ -71,7 +101,6 @@ public class PaasTemplateImp implements PaasTemplateMapper {
 		return list.size()>0?(Integer)list.get(0):0;
 	}
 	//安条件查询信息
-	@Override
 	public List<PaasTemplate> selectObjByCondition(Pageinfo record) {
 		SqlSession session=sqlSessionFactory.openSession();
 		List<PaasTemplate> list=session.selectList("selectPaasTemplateObjByCondit", record);
@@ -79,7 +108,6 @@ public class PaasTemplateImp implements PaasTemplateMapper {
 		return list;
 	}
 	//编辑产品信息
-	@Override
 	public Message updateProduct(PaasTemplate record) {
 		SqlSession session=sqlSessionFactory.openSession();
 		int result=session.update("UpdateProduct",record);
@@ -93,7 +121,6 @@ public class PaasTemplateImp implements PaasTemplateMapper {
 		}
 	}
 
-	@Override
 	public Map<String, List> getTypeAndMode() {
 		Map<String, List> map=new HashMap<String, List>();
 		SqlSession session=sqlSessionFactory.openSession();
@@ -104,25 +131,8 @@ public class PaasTemplateImp implements PaasTemplateMapper {
 		session.close();
 		return map;
 	}
-
 	@Override
-	public List<PaasTemplate> obtainTemplateList(Map paramMap) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public PaasTemplate selectByTemplateId(String id) {
-		return null;
-	}
-	public List<PaasTemplate> selectpaasTemplateList(Integer page, Integer id, String templateId,
-			String templateCategory, Integer counm) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<PaasTemplate> selecttemplateCategory() {
+	public List<String> obtainTemplateCategory() {
 		// TODO Auto-generated method stub
 		return null;
 	}
