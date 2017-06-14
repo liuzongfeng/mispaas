e54b463bc0609120458076ff13ec9837f9d53a06
zhangxing <zhangxing@dayang.com>
2017-06-14 09:53:19 +0800

    zx

--- src/main/java/rest/security/PassSecurity.java
+++ src/main/java/rest/security/PassSecurity.java
@@ -34,17 +34,17 @@ public class PassSecurity extends WebSecurityConfigurerAdapter{
     private CasProperties casProperties;
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-//		http.authorizeRequests()
-//			.antMatchers("/testUploadFile").permitAll()
-//			.antMatchers("/css/**","/index").permitAll()
-//			.antMatchers("/yunweiService/**").hasRole("YUNWEI")
-////			.antMatchers("/passService/**").hasRole("ADMIN")
-//			.and()
-//			.formLogin()
-//			.loginPage("/login").failureUrl("/login-error").successForwardUrl("/hello")
-//			.and()
-//			.csrf().disable()
-//			;
+		http.authorizeRequests()
+			.antMatchers("/testUploadFile").permitAll()
+			.antMatchers("/css/**","/index").permitAll()
+			.antMatchers("/yunweiService/**").hasRole("YUNWEI")
+//			.antMatchers("/passService/**").hasRole("ADMIN")
+			.and()
+			.formLogin()
+			.loginPage("/login").failureUrl("/login-error").successForwardUrl("/hello")
+			.and()
+			.csrf().disable()
+			;
 		http.authorizeRequests()//配置安全策略
 		.antMatchers("/swagger-ui.html","/**/swagger*/**","/**/api*/**","/**/paasService/**").permitAll()
 		.anyRequest().authenticated()
